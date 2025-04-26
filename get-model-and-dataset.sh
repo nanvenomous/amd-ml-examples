923d4df668b4d8807d8842fbb9a54eaea751f192
nanvenomous <mrgarelli@gmail.com>
2025-04-25 21:29:45 -0500

    alter repo for fine tuning only

---
+++ get-model-and-dataset.sh
@@ -0,0 +1,21 @@
+# Authors: Fluid Numerics LLC
+#          Garrett Byrd             (garrett@fluidnumerics.com)
+#          Dr. Joseph Schoonover    (joe@fluidnumerics.com)
+
+# User must have git-lfs installed
+# https://git-lfs.com/
+
+# Get Llama-3-8B (requires permission/authentication)
+# https://ai.meta.com/blog/meta-llama-3/
+
+# git clone https://${HF_USERNAME}:${AUTH_TOKEN}@huggingface.co/meta-llama/Meta-Llama-3-8B
+# git clone https://${HF_USERNAME}:${HF_ACCESS_TOKEN}@huggingface.co/meta-llama/Llama-3.2-3B-Instruct
+git clone https://${HF_USERNAME}:${HF_ACCESS_TOKEN}@huggingface.co/meta-llama/Llama-3.2-3B
+# https://huggingface.co/meta-llama/Llama-3.2-3B-Instruct
+
+# Get MetaMathQA dataset
+# https://meta-math.github.io/
+# git clone https://huggingface.co/datasets/meta-math/MetaMathQA
+
+# You can also download these via the Python API
+# https://huggingface.co/docs/hub/en/models-downloading
